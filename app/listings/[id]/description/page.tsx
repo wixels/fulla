aec9f33bd6f9197a803f4efaa1309e82b63414f9
Dan Sivewright <dan@wixels.com>
2023-06-05 09:59:33 +0200

    setup chatgpt

--- app/listings/[id]/description/page.tsx
+++ app/listings/[id]/description/page.tsx
@@ -1,7 +1,7 @@
-"use client"
-
 import Link from "next/link"
 
+import { Listing } from "@/types/payload-types"
+import { openai } from "@/lib/openai"
 import { buttonVariants } from "@/components/ui/button"
 import { Paragraph } from "@/components/ui/paragraph"
 import { Textarea } from "@/components/ui/textarea"
@@ -9,7 +9,43 @@ import { Title } from "@/components/ui/title"
 import { ListingFooter } from "@/components/listing-footer"
 import { ListingHeader } from "@/components/listings-header"
 
-export default function ListingTitle() {
+async function getListing(id: string): Promise<Listing> {
+  const res = await fetch(
+    `http://localhost:8000/api/listings/${id}?draft=true`,
+    {
+      cache: "no-cache",
+    }
+  )
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
+async function getAiDescription() {
+  const gptreq = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content:
+          "Generate a brief, catch and engaging description to an apartment with 3 bedrooms, 2 bathrooms, 2nd floor, modern stlyish, gas and inverter setup",
+      },
+    ],
+  })
+
+  return gptreq.data.choices[0].message.content
+}
+
+export default async function ListingTitle({
+  params: { id },
+}: {
+  params: { id: string }
+}) {
+  const listing = await getListing(id)
+  const aiDescription = !listing.description ? await getAiDescription() : null
+
+  console.log("aiDescription::: ", aiDescription)
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-start lg:justify-center">
       <ListingHeader />
