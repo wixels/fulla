4a440323e153cba29ccb049873b8279060354afb
Dan Sivewright <dan@wixels.com>
2023-06-21 11:18:57 +0200

    auth change

--- lib/auth.ts
+++ lib/auth.ts
@@ -41,7 +41,7 @@ export const authOptions: NextAuthOptions = {
             equals: token.email,
           },
         },
-      } as Prisma.UserFindFirstArgs)
+      })
 
       if (!dbUser) {
         if (user) {
