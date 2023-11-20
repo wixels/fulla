1c7d4761de2ee7deb53cc513f2de096d9147581f
Dan Sivewright <dan@wixels.com>
2023-11-20 12:50:56 +0200

    rever to old nav

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -1,44 +1,100 @@
-import Link from "next/link"
+"use client"
 
+import { useMemo, useState } from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
+
+import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 
 import { Icons } from "../icons"
-import { buttonVariants } from "../ui/button"
 import { ProfileAvatar } from "./profile-avatar"
 import { Search } from "./search"
 
-type Props = {
-  active?: string
-}
-export const Header: React.FC<Props> = ({ active }) => {
+export function Header() {
+  const path = usePathname()
+  const [activeTab, setActiveTab] = useState(path ?? siteConfig.mainNav[0].href)
+  const [hoveredTab, setHoveredTab] = useState<string | null>(
+    path ?? siteConfig.mainNav[0].href
+  )
+
+  const hideMe = useMemo(() => {
+    if (path?.includes("/create/space")) return true
+    if (path?.includes("/apply")) return true
+    return false
+  }, [path])
+
   return (
     <header
       className={cn(
-        "gutter sticky top-0 z-10 grid grid-cols-3 items-center gap-3 bg-background py-2"
+        "gutter sticky top-0 z-10 grid grid-cols-3 items-center gap-3 bg-background py-2",
+        {
+          hidden: hideMe,
+        }
       )}
     >
       <div className="flex items-center gap-3">
         <Icons.logo className="h-6 w-6" />
-        <Link
-          href={"/"}
-          className={buttonVariants({
-            size: "xs",
-            rounded: "full",
-            variant: active === "/" ? "default" : "ghost",
-          })}
+        <motion.div
+          onHoverEnd={() => setHoveredTab(activeTab)}
+          className="hidden space-x-1 lg:flex"
         >
-          Browse
-        </Link>
-        <Link
-          href={"/collections"}
-          className={buttonVariants({
-            size: "xs",
-            rounded: "full",
-            variant: active === "/collections" ? "default" : "ghost",
-          })}
-        >
-          Collections
-        </Link>
+          {siteConfig.mainNav.map(({ href, title }) => (
+            <Link
+              href={href}
+              key={href}
+              onClick={() => setActiveTab(href)}
+              className={cn(
+                "relative rounded-full px-3 py-1.5 text-sm font-medium text-white transition hover:text-white/50 focus-visible:outline-2"
+              )}
+            >
+              <motion.div onHoverStart={() => setHoveredTab(href)}>
+                {activeTab === href && (
+                  <motion.div
+                    layoutId="pill"
+                    className="absolute inset-0 bg-foreground"
+                    style={{
+                      borderRadius: 9999,
+                    }}
+                    transition={{
+                      type: "spring",
+                      bounce: 0.2,
+                      duration: 0.6,
+                    }}
+                  />
+                )}
+                {hoveredTab === href && (
+                  <motion.div
+                    layoutId="hover"
+                    className="absolute inset-0 bg-stone-600/10"
+                    style={{
+                      borderRadius: 9998,
+                    }}
+                    animate={{
+                      opacity: 1,
+                    }}
+                    exit={{
+                      opacity: 0,
+                    }}
+                    transition={{
+                      type: "spring",
+                      bounce: 0.2,
+                      duration: 0.6,
+                    }}
+                  />
+                )}
+                <span
+                  className={cn("relative z-10 mix-blend-exclusion", {
+                    "text-white": activeTab === href,
+                  })}
+                >
+                  {title}
+                </span>
+              </motion.div>
+            </Link>
+          ))}
+        </motion.div>
       </div>
       <Search />
       <div className="flex items-center justify-end gap-3">
