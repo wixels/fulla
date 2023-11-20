a93a92ff06169305e368284dac8c229c66c84aaf
Dan Sivewright <dan@wixels.com>
2023-11-20 12:53:57 +0200

    remove unused props for header

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,14 +1,9 @@
-"use client"
-
-import { usePathname } from "next/navigation"
-
 import { Header } from "@/components/navigation/header"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const path = usePathname()
   return (
     <div className="flex min-h-screen flex-col">
-      <Header active={path} />
+      <Header />
       {children}
     </div>
   )
