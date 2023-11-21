90a1300161b9ab90d4dfaa59f09597ffec74e122
Dan Sivewright <dan@wixels.com>
2023-11-21 12:43:42 +0200

    no footer on org side

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -4,6 +4,7 @@ import Balancer from "react-wrap-balancer"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { Paragraph } from "@/components/ui/paragraph"
 import { Title } from "@/components/ui/title"
+import { Footer } from "@/components/navigation/footer"
 import { Header } from "@/components/navigation/header"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -11,6 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({ children, modal }: RootLayoutProps) {
           <Providers>
             {children}
             {modal}
-            <Footer />
+
             <Toaster />
             <TailwindIndicator />
           </Providers>
