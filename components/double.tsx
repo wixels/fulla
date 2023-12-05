76c46c0b6957bb597e5d17cff1c8a183bbb0867d
Dan Sivewright <dan@wixels.com>
2023-12-05 12:15:31 +0200

    all marketing stuff

--- components/double.tsx
+++ components/double.tsx
@@ -15,37 +15,40 @@ type Props = {
   reversed?: boolean
 }
 
-export const Double: React.FC<Props> = ({ projects, reversed = false }) => {
-  const firstImage = useRef(null)
-  const secondImage = useRef(null)
-  let requestAnimationFrameId = null
-  let xPercent = reversed ? 100 : 0
-  let currentXPercent = reversed ? 100 : 0
+export const Double: React.FC<Props> = ({ reversed = false }) => {
+  const firstImage = useRef<HTMLDivElement>(null) // Add type annotation for the ref
+  const secondImage = useRef<HTMLDivElement>(null) // Add type annotation for the ref
+  let requestAnimationFrameId: number | null = null // Add type annotation for the variable
+  let xPercent = 0
+  let currentXPercent = 0
   const speed = 0.15
 
-  const manageMouseMove = (e) => {
+  const manageMouseMove = (e: React.MouseEvent) => {
     const { clientX } = e
     xPercent = (clientX / window.innerWidth) * 100
 
-    if (!requestAnimationFrameId) {
+    if (requestAnimationFrameId === null) {
       requestAnimationFrameId = window.requestAnimationFrame(animate)
     }
   }
 
   const animate = () => {
-    //Add easing to the animation
+    // Add easing to the animation
     const xPercentDelta = xPercent - currentXPercent
     currentXPercent = currentXPercent + xPercentDelta * speed
 
-    //Change width of images between 33.33% and 66.66% based on cursor
+    // Change width of images between 33.33% and 66.66% based on cursor
     const firstImagePercent = 66.66 - currentXPercent * 0.33
     const secondImagePercent = 33.33 + currentXPercent * 0.33
-    console.log(secondImagePercent)
-    firstImage.current.style.width = `${firstImagePercent}%`
-    secondImage.current.style.width = `${secondImagePercent}%`
 
-    if (Math.round(xPercent) == Math.round(currentXPercent)) {
-      window.cancelAnimationFrame(requestAnimationFrameId)
+    // Check if refs are not null before accessing their properties
+    if (firstImage.current && secondImage.current) {
+      firstImage.current.style.width = `${firstImagePercent}%`
+      secondImage.current.style.width = `${secondImagePercent}%`
+    }
+
+    if (Math.round(xPercent) === Math.round(currentXPercent)) {
+      window.cancelAnimationFrame(requestAnimationFrameId!) // Use the non-null assertion operator
       requestAnimationFrameId = null
     } else {
       window.requestAnimationFrame(animate)
@@ -64,7 +67,7 @@ export const Double: React.FC<Props> = ({ projects, reversed = false }) => {
           <Image className="object-cover" src={ImageOne} alt={"image"} fill />
         </div>
         <div className="p-3">
-          <Title className="font-bold font-mono" level={3} showAs={6}>
+          <Title className="font-mono font-bold" level={3} showAs={6}>
             On-Demand Desk
           </Title>
           <p>Book desks when you need them, where you need them.</p>
@@ -76,7 +79,7 @@ export const Double: React.FC<Props> = ({ projects, reversed = false }) => {
           <Image src={ImageTwo} className="object-cover" fill alt={"image"} />
         </div>
         <div className="p-3">
-          <Title className="font-bold font-mono" level={3} showAs={6}>
+          <Title className="font-mono font-bold" level={3} showAs={6}>
             Meeting Spaces
           </Title>
           <p>Conduct meetings in style – reserve our modern meeting rooms.</p>
